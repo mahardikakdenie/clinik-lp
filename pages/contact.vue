f5c647ada506422d4bb011e76b1e99e5b3bf56a0
mahardikakdenie <kucingemas744@gmail.com>
2025-07-31 10:13:11 +0700

    contact

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,15 +12,15 @@
 
                 <div class="grid grid-cols-2 mt-5 gap-4">
                     <div class="flex flex-col items-center justify-center">
-                        <span class="bg-[#1abaa6] text-master text-md sm:text-2xl rounded-2xl sm:px-20 px-5 py-3 text-white">
+                        <a class="bg-[#1abaa6] text-master text-md sm:text-2xl rounded-2xl sm:px-20 px-5 py-3 text-white">
                             Whatsapp
-                        </span>
+                        </a>
 
-                        <div class="flex items-center">
+                        <div class="flex items-center sm:mr-16 mr-8">
                             <img src="/icon-whatsapp.png" class="sm:w-20 sm:h-20 w-10 h-10" alt="">
-                            <span class="text-[#1abaa6] text-sm">
+                            <a class="text-[#1abaa6] text-sm hover:underline" href="https://wa.me/6282211776167" target="_blank">
                                 0822-1177-6167
-                            </span>
+                            </a>
                         </div>
                     </div>
                     <div class="flex justify-center items-center flex-col">
@@ -29,9 +29,9 @@
                         </span>
                          <div class="flex items-center">
                             <img src="/icon-shopee.png" class="sm:w-20 sm:h-20 w-10 h-10" alt="">
-                            <span class="text-[#1abaa6] text-sm">
+                            <a class="text-[#1abaa6] text-sm hover:underline" href="https://shopee.co.id/drf.skincare" target="_blank">
                                 @DrF Skincare
-                            </span>
+                            </a>
                         </div>
                     </div>
                     <div class="flex justify-center items-center flex-col mx-3">
@@ -40,9 +40,9 @@
                         </span>
                          <div class="flex items-center">
                             <img src="/icon-instagram.png" class="sm:w-20 sm:h-20 w-10 h-10" alt="">
-                            <span class="text-[#1abaa6] text-sm">
+                            <a class="text-[#1abaa6] text-sm hover:underline" href="https://www.instagram.com/maidaskinclinic.bandung/" target="_blank">
                                 @maidaskinclinic.bandung
-                            </span>
+                            </a>
                         </div>
                     </div>
                     <div class="flex justify-center items-center flex-col">
@@ -51,9 +51,9 @@
                         </span>
                          <div class="flex items-center">
                             <img src="/icon-tokopedia.png" class="sm:w-20 sm:h-20 w-10 h-10" alt="">
-                            <span class="text-[#1abaa6] text-sm">
+                            <a class="text-[#1abaa6] text-sm hover:underline" href="https://www.tokopedia.com/drfskincare" target="_blank">
                                 @DrF Skincare
-                            </span>
+                            </a>
                         </div>
                     </div>
                 </div>
