2a336ff2f637b3972da6641b411b3eca73aca580
mahardikakdenie <kucingemas744@gmail.com>
2025-08-06 21:02:07 +0700

    tidyup box

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,4 +6,11 @@
     </div>
 </template>
 
-<script></script>
+
+<script setup lang="ts">
+useSeoMeta({
+	title: 'Maida Skin Clinic - Cantik Alami, Bebas Ketergantungan',
+	description:
+		'Perawatan kulit personalisasi dengan bahan alami dan teknologi aman.',
+});
+</script>
